414ae15e853a71d34d45b0d14de537a80aeb08a8
cli <82491956+xa1on@users.noreply.github.com>
2026-02-16 17:31:48 -0800

    feat: basic class structure for lemonflow

--- src/main.py
+++ src/main.py
@@ -2,34 +2,51 @@ import sounddevice as sd
 import numpy as np
 import scipy.io.wavfile as wav
 import io
+from io import BytesIO
 
 from openai import OpenAI
 
 SAMPLE_RATE = 44100
+BASE_URL = "http://localhost:8000/api/v1"
+DEFAULT_MODEL = "Whisper-Tiny"
 
+class LemonFlow:
+
+    def __init__(self, sample_rate: int=SAMPLE_RATE, base_url=BASE_URL):
+        self.sample_rate = sample_rate
+        self.client = OpenAI(
+            base_url=base_url,
+            api_key="lemonflow"
+        )
+
+    def save_audio(self, inp_audio) -> BytesIO:
+        byte_io = io.BytesIO()
+        wav.write(byte_io, self.sample_rate, inp_audio)
+        byte_io.seek(0)
+        byte_io.name = "chunk.wav"
+        return byte_io
+    
+    def transcribe_io(self, byte_io: BytesIO, model: str=DEFAULT_MODEL) -> str:
+        print("transcribing")
+        transcript = self.client.audio.transcriptions.create(
+            model=model,
+            file=byte_io
+        )
+        return transcript.text
+
+    def transcribe(self, inp_audio, model: str=DEFAULT_MODEL) -> str:
+        return self.transcribe_io(self.save_audio(inp_audio), model)
+    
+    def start_recording(self, length: int=10):
+        print("recording started")
+        recording = sd.rec(int(length * self.sample_rate), samplerate=self.sample_rate, channels=1, dtype='int16')
+        sd.wait()
+        print("recording ended")
+        print(self.transcribe(recording))
 
 def main():
-    print("loading model")
-    client = OpenAI(
-        base_url="http://localhost:8000/api/v1",
-        api_key="lemonade"
-    )
-    print("model loaded")
-    print("recording...")
-    recording = sd.rec(int(10 * SAMPLE_RATE), samplerate=SAMPLE_RATE, channels=1, dtype='int16')
-    sd.wait()
-    print("recording ended.")
-
-    byte_io = io.BytesIO()
-    wav.write(byte_io, SAMPLE_RATE, recording)
-    byte_io.seek(0)
-    byte_io.name = "chunk.wav"
-    print("transcribing...")
-    transcript = client.audio.transcriptions.create(
-        model="Whisper-Tiny",
-        file=byte_io
-    )
-    print(transcript.text.strip())
+    lemon_flow = LemonFlow()
+    lemon_flow.start_recording()
 
 if __name__ == "__main__":
     main()
